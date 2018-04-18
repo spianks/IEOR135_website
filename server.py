49a991041d9c4db2a8e8746750d336dfa53645b5
Seoyeon Park <spark@Seoyeons-MacBook-Air.local>
2018-04-18 09:06:58 -0700

    website put together

--- server.py
+++ server.py
@@ -7,13 +7,33 @@ def main():
 	print("got to main")
 	return render_template('index.html')
 
-@app.route('/post', methods=['GET', 'POST'])
-def post(pic):
-	### run ML shit here
-	print("got to post")
-	return render_template('post_result.html')
+# @app.route('/post', methods=['GET', 'POST'])
+# def post(pic):
+# 	### run ML shit here
+# 	print("got to post")
+# 	return render_template('post_result.html')
 
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
+
+
+
+# from flask import Flask, render_template, request
+# from werkzeug import secure_filename
+# app = Flask(__name__)
+
+# @app.route('/upload')
+# def upload_file():
+#    return render_template('upload.html')
+	
+# @app.route('/uploader', methods = ['GET', 'POST'])
+# def upload_file():
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       f.save(secure_filename(f.filename))
+#       return 'file uploaded successfully'
+		
+# if __name__ == '__main__':
+#    app.run(debug = True)
